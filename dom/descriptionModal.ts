4337453826dc0c1a2f4c931a8fbb73e03a4141ba
liolle <44002141+liolle@users.noreply.github.com>
2024-02-11 00:34:32 +0100

    [Update] DescriptionModal - release (eventListener) resource on modal close

--- dom/descriptionModal.ts
+++ dom/descriptionModal.ts
@@ -3,6 +3,8 @@ import { Modal, App } from "obsidian";
 export class DescriptionModal extends Modal {
 	private result: string | undefined;
 	private onSubmit: (result: string | undefined) => void;
+	private text_area: HTMLTextAreaElement;
+	private submit_btn: HTMLButtonElement;
 
 	constructor(
 		app: App,
@@ -14,6 +16,16 @@ export class DescriptionModal extends Modal {
 		this.result = text;
 	}
 
+	inputHandler(event: Event) {
+		const text_area = event.target as HTMLTextAreaElement;
+		this.result = text_area.value;
+	}
+
+	submitHandler(event: Event) {
+		this.onSubmit(this.result);
+		this.close();
+	}
+
 	onOpen() {
 		const { contentEl } = this;
 
@@ -21,28 +33,31 @@ export class DescriptionModal extends Modal {
 			cls: ["description-modal-body"],
 		});
 
-		const input = body.createEl("textarea", {
+		this.text_area = body.createEl("textarea", {
 			cls: ["description-modal-input"],
 		});
+		this.text_area.addEventListener("input", this.inputHandler.bind(this));
+		this.text_area.value = this.result || "";
 
-		const lambda = () => {
-			this.result = input.value;
-		};
-
-		input.addEventListener("input", lambda);
-		input.value = this.result || "";
-
-		const submit_btn = createEl("button", { text: "Save" });
-		submit_btn.addEventListener("click", (event) => {
-			input.removeEventListener("input", lambda);
-			this.close();
-			this.onSubmit(this.result);
-		});
-		body.appendChild(submit_btn);
+		this.submit_btn = createEl("button", { text: "Save" });
+		this.submit_btn.addEventListener(
+			"click",
+			this.submitHandler.bind(this)
+		);
+		body.appendChild(this.submit_btn);
+		console.log(this);
 	}
 
 	onClose() {
-		const { contentEl } = this;
+		const { contentEl, text_area, submit_btn } = this;
 		contentEl.empty();
+
+		if (text_area) {
+			this.text_area.removeEventListener("input", this.inputHandler);
+		}
+
+		if (submit_btn) {
+			this.submit_btn.removeEventListener("click", this.submitHandler);
+		}
 	}
 }
