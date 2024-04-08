6ca73d6ebc92a24363391d28d15015b44c337ff9
liolle <44002141+liolle@users.noreply.github.com>
2024-04-09 01:28:22 +0200

    [Update] DOMHandler - delete unused code - add populateNodes + resources clean up

--- dom/handler.ts
+++ dom/handler.ts
@@ -1,126 +1,51 @@
-import { FileHandler } from "io/fileHandler";
-import Crafty, { CraftyNode } from "main";
-import { debounce } from "obsidian";
+import Crafty from "main";
+import { CraftyNode } from "observers/observer";
 
 export class DOMHandler {
-	private static selection_listeners_free: (() => void)[] = [];
+	private static selection_listeners_cb: (() => void)[] = [];
+	private static nodes_click_lister_cb: (() => void)[] = [];
 
 	static #freeSelectionListeners() {
-		let callback = this.selection_listeners_free.pop();
+		let callback = this.selection_listeners_cb.pop();
 		while (callback) {
 			callback();
-			callback = this.selection_listeners_free.pop();
+			callback = this.selection_listeners_cb.pop();
 		}
 	}
 
-	static #showSelection(
-		Plugin: Crafty,
-		selected_node: { name: string; value: CraftyNode },
-		container: HTMLElement
-	) {
-		this.#freeSelectionListeners();
-		const title = this.#titleFromNode(selected_node.value);
-
-		const header_area = container.createEl("div", {
-			cls: ["description-header-div"],
-		});
-		header_area.createEl("span", {
-			text: title,
-			cls: [],
-		});
-		const body = container.createEl("div", {
-			cls: ["description-modal-body"],
-		});
-
-		const text_area = body.createEl("textarea", {
-			cls: ["description-modal-input"],
-		});
-
-		body.createEl("span", {
-			text: "Saved",
-			cls: ["save_state"],
-		});
-		const inputChangeCallback = debounce(
-			(event: Event) => {
-				this.#saveDescription(Plugin, text_area, selected_node.value);
-			},
-			1000,
-			true
-		);
-		//@ts-ignore
-		text_area.inputChangeCallback = inputChangeCallback;
-		text_area.addEventListener("input", inputChangeCallback);
-		text_area.value = selected_node.value.description || "";
-		this.selection_listeners_free.push(() => {
-			text_area.removeEventListener(
-				"input",
-				//@ts-ignore
-				inputChangeCallback
-			);
-		});
-	}
-
-	static async #saveDescription(
-		plugin: Crafty,
-		text_area: HTMLTextAreaElement,
-		node: CraftyNode
-	) {
-		const file = plugin.app.workspace.getActiveFile();
-		if (!file) return;
-
-		const description = text_area.value;
-
-		if (!description) {
-			delete node.description;
-		} else {
-			node.description = description;
-		}
-		const save_state = document.querySelector(".save_state");
-		if (save_state) {
-			save_state.textContent = "Saving...";
-		}
-		await FileHandler.updateCanvasNode(node, file, plugin.app.vault);
-		if (save_state) {
-			setTimeout(() => {
-				save_state.textContent = "Saved";
-			}, 200);
-		}
-		if (plugin.canvasLeaf) {
-			this.attachToolTip(plugin);
-			plugin.changeLeafFocus(plugin.canvasLeaf, true);
-			setTimeout(() => {
-				plugin.changeLeafFocus(plugin.leaf, true);
-			}, 100);
+	static #freeNodesClickListeners() {
+		let callback = this.nodes_click_lister_cb.pop();
+		while (callback) {
+			callback();
+			callback = this.nodes_click_lister_cb.pop();
 		}
 	}
 
-	static #titleFromNode(node: CraftyNode) {
-		const title_character_limit = 48;
-		let title = "";
-		let file = [];
+	static async populateNodes(nodes: CraftyNode[] | null) {
+		const body = document.querySelector(".nodes-body");
+		if (!nodes || !body) return;
+		DOMHandler.#freeNodesClickListeners();
+		body.empty();
 
-		switch (node.type) {
-			case "text":
-				if (!node.text || /^\s*$/.test(node.text)) title = "Untitled";
-				else title = node.text;
-				break;
+		for (const node of nodes) {
+			const cls = ["node-element"];
+			if (node.selected) cls.push("node-active");
+			const child = createEl("div", {
+				text: node.title,
+				attr: { class: cls.join(" ") },
+			});
 
-			case "file":
-				//@ts-ignore
-				file = node.file.split("/");
-				title = file[file.length - 1];
-				break;
+			const clickCallback = (event: Event) => {
+				node.container.click();
+			};
+			child.addEventListener("click", clickCallback);
 
-			case "group":
-				title = node.label as string;
-				break;
+			this.nodes_click_lister_cb.push(() => {
+				child.removeEventListener("click", clickCallback);
+			});
 
-			default:
-				break;
+			body.appendChild(child);
 		}
-		return title.length > title_character_limit
-			? `${title.slice(0, title_character_limit)}`
-			: title;
 	}
 
 	static attachToolTip(plugin: Crafty) {
