783e2b9556f869ad246919b46ed33149e4d96248
liolle <44002141+liolle@users.noreply.github.com>
2024-04-10 23:25:47 +0200

    [Update] FileHandler

--- io/fileHandler.ts
+++ io/fileHandler.ts
@@ -1,36 +1,20 @@
-import { CraftyNode } from "main";
+import { CraftyNode } from "observers/observer";
 import { TFile, Vault } from "obsidian";
-
-export interface RawCraftyNode {
-	file?: string;
-	description?: string;
-	id: string;
-	text?: string;
-	type: string;
-	height: number;
-	width: number;
-	x: number;
-	y: number;
-}
 
 export class FileHandler {
 	static async updateCanvasNode(node: CraftyNode, file: TFile, vault: Vault) {
 		const content = await vault.read(file);
-
 		try {
 			const parsed_data = JSON.parse(content);
-
 			for (const elem of parsed_data.nodes) {
 				if (elem.id == node.id) {
 					delete elem.description;
-
 					if (node.description) {
 						elem.description = node.description;
 					}
 					break;
 				}
 			}
-
 			await vault.modify(file, JSON.stringify(parsed_data));
 		} catch (error) {
 			console.error(error);
