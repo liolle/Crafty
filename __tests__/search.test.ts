f58d5dd2cfe68c60f26991c399be9280cd567b85
liolle <44002141+liolle@users.noreply.github.com>
2024-05-09 02:48:49 +0200

    [Update] explorer tests

--- __tests__/search.test.ts
+++ __tests__/search.test.ts
@@ -1,8 +1,54 @@
 //Search tests
 
 // Sort tests
-import { expect, test } from "vitest";
+import { CraftyNode, NodesExplorer } from "nodes/nodes";
+import { beforeEach, describe, expect, test } from "vitest";
 
-test("adds 1 + 2 to equal 3", () => {
-	expect(3).toBe(3);
+const sample_nodes: CraftyNode[] = [
+	{
+		id: "s1",
+		title: "node1",
+		description: "",
+		container: null,
+		selected: false,
+		type: "canvas",
+	},
+];
+
+describe("NodesExplorer", () => {
+	const explorer = new NodesExplorer();
+
+	beforeEach(() => {
+		explorer.clear();
+	});
+
+	test("Simple add", () => {
+		expect(explorer.size).toBe(0);
+
+		explorer.add(sample_nodes[0]);
+
+		expect(explorer.size).toBe(1);
+	});
+
+	test("Simple remove", () => {
+		expect(explorer.size).toBe(0);
+
+		const node = sample_nodes[0];
+		explorer.add(node);
+		explorer.remove(node.title);
+
+		expect(explorer.size).toBe(0);
+	});
+
+	test("Simple search", () => {
+		expect(explorer.size).toBe(0);
+
+		const node = sample_nodes[0];
+		explorer.add(node);
+
+		const search = explorer.search(node.title);
+		const s_node = search[0];
+		expect(search.length).toBe(1);
+		expect(s_node.title).toBe(node.title);
+	});
 });
